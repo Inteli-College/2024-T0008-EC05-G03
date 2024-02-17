cfa05d20d3019fb1c912df889bf767e74e35a8a0
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2024-02-16 22:05:25 -0300

    fix: docosaurus.config.js

--- documentacao/docusaurus.config.js
+++ documentacao/docusaurus.config.js
@@ -92,15 +92,6 @@ const config = {
         style: 'dark',
         links: [
           {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
             title: 'Community',
             items: [
               {
@@ -120,10 +111,6 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
               {
                 label: 'GitHub',
                 href: 'https://github.com/facebook/docusaurus',

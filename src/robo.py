e0d68780edcef452448983bd6f035ac632076b9f
gustavoesteves0 <gustavo.esteves@sou.inteli.edu.br>
2024-02-26 17:59:40 -0300

    Update robo.py

--- src/robo.py
+++ src/robo.py
@@ -14,24 +14,25 @@ spinner = yaspin(text="Processando...", color="yellow")
 available_ports = list_ports.comports()
 
 home = (240.53, 0, 150.23, 0)
-#55.64
-remedio1 = (346.51, 57.66, -35.85, 9.45)
-remedio2 = (336.84, -8.5, -21.46, -1.44)
-remedio3 = (329.34, -71.42, -21.63, -12.24)
-remedio4 = (324.91, -142.74, -20.37, -23.72)
-remedio5 = (260.07, 67.16, -23.11, 14.48)
-remedio6 = (257.25, 3.19, -13.3, 0.71)
-remedio7 = (251.22, -67.73, -15.85, -15.09)
-remedio8 = (241.53, -132.68, -15.22, -28.78)
+#Posição das caixas é a 10cm da base do robô
+#55.64 #tamanho de 4 bloquinhos no z = -10.18
+remedio1 = (111.46, -294.44, -10.18, -70.86)
+remedio2 = (32.09, -296.95, -10.18, -83.83)
+remedio3 = (-31.74, -299.74, -10.18, -96.05)
+remedio4 = (-98.01, -295.91, -10.18, -108.33)
+remedio5 = (111.59, -217.81, -7.61, -62.87)
+remedio6 = (44.38, -210.17, -7.28, -78.08)
+remedio7 = (-24.8, -209.59, -8.17, -96.75)
+remedio8 = (-91.94, -212.67, -6.64, -113.38)
 
-destino1 = (3.85, -295.69, 6.37, -89.25)
-destino2 = (-66.26, -288.2, 6.37, -102.95)
-destino3 = (-133, -273.53, 6.37, -115.93)
-destino4 = (-200.93, -276.96, 6.37, -125.96)
-destino5 = (7.77, -202.01, 6.37, -87.8)
-destino6 = (-61.54, -206.96, 6.37, -106.56)
-destino7 = (-131.09, -206.83, 6.37, -122.37)
-destino8 = (-189.24, -196.85, 6.37, -133.87)
+destino1 = (106.46, 205.79, 43.29, 62.65)
+destino2 = (35.55, 217.71, 18.44, 80.73)
+destino3 = (-27.28, 215.22, 19.69, 97.22)
+destino4 = (-97.49, 213.63, 13.13, 114.53)
+destino5 = (110.93, 302.16, 12.63, 69.84)
+destino6 = (37.7, 301.52, 14.95, 82.87)
+destino7 = (-27.07, 305.53, 12.42, 95.06)
+destino8 = (-98.12, 301.37, 13.09, 108.03)
 
 # Pede para o usuário escolher uma das portas disponíveis
 porta_escolhida = inquirer.prompt([
@@ -58,29 +59,101 @@ spinner.stop()
 #y1 = int(Y1["y1"])
 #z1 = int(Z1["z1"])
 
-# Move o robô para a posição desejada
+# Fazer com que todos subam mais antes de deslocar o remedio
+# Faz com que o robô pegue o remédio 1 e coloque no destino 1.
 spinner.start()
 robo.move_to_J(remedio1[0], remedio1[1], 55.64, remedio1[3], wait=True)
 robo.move_to(remedio1[0], remedio1[1], -9.34, remedio1[3], wait=True)
 robo.suck(True)
+robo.wait(200)
 robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
 robo.move_to_J(destino1[0], destino1[1], destino1[2], destino1[3], wait=True)
 robo.suck(False)
 robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
 spinner.stop()
 
-# Inicializa o efetuador do robô
+# Faz com que o robô pegue o remédio 2 e coloque no destino 2. #Remedio 2 ta rente
 spinner.start()
+robo.move_to_J(remedio2[0], remedio2[1], 55.64, remedio2[3], wait=True)
+robo.move_to(remedio2[0], remedio2[1], -9.34, remedio2[3], wait=True)
 robo.suck(True)
-# Adiciona um delay para o robô efetuar a operação
 robo.wait(200)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+robo.move_to_J(destino2[0], destino2[1], destino2[2], destino2[3], wait=True)
+robo.suck(False)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
 spinner.stop()
 
-# Inicializa o efetuador do robô
+# Faz com que o robô pegue o remédio 3 e coloque no destino 3. 3 ta descentralizado
 spinner.start()
-robo.suck(False)
-# Adiciona um delay para o robô efetuar a operação
+robo.move_to_J(remedio3[0], remedio3[1], 55.64, remedio3[3], wait=True)
+robo.move_to(remedio3[0], remedio3[1], -9.34, remedio3[3], wait=True)
+robo.suck(True)
 robo.wait(200)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+robo.move_to_J(destino3[0], destino3[1], destino3[2], destino3[3], wait=True)
+robo.suck(False)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+spinner.stop()
+
+# Faz com que o robô pegue o remédio 4 e coloque no destino 4. 4 ta descentralizado e rente
+spinner.start()
+robo.move_to_J(remedio4[0], remedio4[1], 55.64, remedio4[3], wait=True)
+robo.move_to(remedio4[0], remedio4[1], -9.34, remedio4[3], wait=True)
+robo.suck(True)
+robo.wait(200)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+robo.move_to_J(destino4[0], destino4[1], destino4[2], destino4[3], wait=True)
+robo.suck(False)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+spinner.stop()
+
+# Faz com que o robô pegue o remédio 5 e coloque no destino 5. 
+spinner.start()
+robo.move_to_J(remedio5[0], remedio5[1], 55.64, remedio5[3], wait=True)
+robo.move_to(remedio5[0], remedio5[1], -9.34, remedio5[3], wait=True)
+robo.suck(True)
+robo.wait(200)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+robo.move_to_J(destino5[0], destino5[1], destino5[2], destino5[3], wait=True)
+robo.suck(False)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+spinner.stop()
+
+# Faz com que o robô pegue o remédio 6 e coloque no destino 6.
+spinner.start()
+robo.move_to_J(remedio6[0], remedio6[1], 55.64, remedio6[3], wait=True)
+robo.move_to(remedio6[0], remedio6[1], -9.34, remedio6[3], wait=True)
+robo.suck(True)
+robo.wait(200)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+robo.move_to_J(destino6[0], destino6[1], destino6[2], destino6[3], wait=True)
+robo.suck(False)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+spinner.stop()
+
+# Faz com que o robô pegue o remédio 7 e coloque no destino 7.
+spinner.start()
+robo.move_to_J(remedio7[0], remedio7[1], 55.64, remedio7[3], wait=True)
+robo.move_to(remedio7[0], remedio7[1], -9.34, remedio7[3], wait=True)
+robo.suck(True)
+robo.wait(200)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+robo.move_to_J(destino7[0], destino7[1], destino7[2], destino7[3], wait=True)
+robo.suck(False)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+spinner.stop()
+
+# Faz com que o robô pegue o remédio 8 e coloque no destino 8.
+spinner.start()
+robo.move_to_J(remedio8[0], remedio8[1], 55.64, remedio8[3], wait=True)
+robo.move_to(remedio8[0], remedio8[1], -9.34, remedio8[3], wait=True)
+robo.suck(True)
+robo.wait(200)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+robo.move_to_J(destino8[0], destino8[1], destino8[2], destino8[3], wait=True)
+robo.suck(False)
+robo.move_to_J(home[0], home[1], home[2], home[3], wait=True)
 spinner.stop()
 
 # Pega a posição atual do robô

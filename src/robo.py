09f64ac699156517ea8c3bda15e0fe3e839b32c0
gustavoesteves0 <gustavo.esteves@sou.inteli.edu.br>
2024-02-28 11:18:50 -0300

    create pontos.json

--- src/robo.py
+++ src/robo.py
@@ -1,7 +1,28 @@
+import csv
+import json
 from serial.tools import list_ports
 import inquirer
 import pydobot  
 from yaspin import yaspin
+
+#home = (240.53, 0, 150.23, 0)
+#remedio1 = (111.46, -294.44, -9.18, -70.86)
+#remedio2 = (32.09, -296.95, -10.18, -83.83)
+#remedio3 = (-31.74, -299.74, -10.18, -96.05)
+#remedio4 = (-98.01, -295.91, -10.18, -108.33)
+#remedio5 = (111.59, -217.81, -7.61, -62.87)
+#remedio6 = (44.38, -210.17, -7.28, -78.08)
+#remedio7 = (-24.8, -209.59, -8.17, -96.75)
+#remedio8 = (-91.94, -212.67, -6.64, -113.38)
+
+#destino1 = (106.46, 205.79, 43.29, 62.65)
+#destino2 = (35.55, 217.71, 18.44, 80.73)
+#destino3 = (-27.28, 215.22, 19.69, 97.22)
+#destino4 = (-97.49, 213.63, 13.13, 114.53)
+#destino5 = (110.93, 302.16, 12.63, 69.84)
+#destino6 = (37.7, 301.52, 14.95, 82.87)
+#destino7 = (-27.07, 305.53, 12.42, 95.06)
+#destino8 = (-98.12, 301.37, 13.09, 108.03)
 
 # Traz o spinner para apresentar uma animação enquanto o robô está se movendo
 spinner = yaspin(text="Processando...", color="yellow")
@@ -17,27 +38,6 @@ device = pydobot.Dobot(port=porta_escolhida, verbose=False)
 # Define a velocidade e a aceleracao do robô
 device.speed(30, 30)
 
-home = (240.53, 0, 150.23, 0)
-#Posição das caixas é a 10cm da base do robô
-#55.64 #tamanho de 4 bloquinhos no z = -10.18
-remedio1 = (111.46, -294.44, -9.18, -70.86)
-remedio2 = (32.09, -296.95, -10.18, -83.83)
-remedio3 = (-31.74, -299.74, -10.18, -96.05)
-remedio4 = (-98.01, -295.91, -10.18, -108.33)
-remedio5 = (111.59, -217.81, -7.61, -62.87)
-remedio6 = (44.38, -210.17, -7.28, -78.08)
-remedio7 = (-24.8, -209.59, -8.17, -96.75)
-remedio8 = (-91.94, -212.67, -6.64, -113.38)
-
-destino1 = (106.46, 205.79, 43.29, 62.65)
-destino2 = (35.55, 217.71, 18.44, 80.73)
-destino3 = (-27.28, 215.22, 19.69, 97.22)
-destino4 = (-97.49, 213.63, 13.13, 114.53)
-destino5 = (110.93, 302.16, 12.63, 69.84)
-destino6 = (37.7, 301.52, 14.95, 82.87)
-destino7 = (-27.07, 305.53, 12.42, 95.06)
-destino8 = (-98.12, 301.37, 13.09, 108.03)
-
 caminho = []
 destino = []
 
@@ -47,6 +47,19 @@ def __init__():
         device.move_to_J(home[0], home[1], home[2], home[3], wait=True)
         spinner.stop()
         return "Robô inicializado."
+
+def load_points_from_file(file_path):
+    # Lógica para carregar pontos do arquivo
+    with open(file_path, 'r') as file:
+        if file_path.endswith('.json'):
+            data = json.load(file)
+            return data['home'], data['remedios'], data['destinos']  # Retorna os pontos dos remédios e destinos
+        elif file_path.endswith('.csv'):
+            reader = csv.reader(file)
+            data = [(float(row[0]), float(row[1])) for row in reader]
+            return data
+        else:
+            raise ValueError("Formato de arquivo não suportado")
 
 def casa():
         spinner.start()
@@ -72,7 +85,7 @@ def execute_comando(comando):
     match comando:
         case "home":
             spinner.start()
-            device.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+            device.move_to_J(home['x'], home['y'], home['z'], home['r'], wait=True)
             spinner.stop()
             return "Robô retornou à posição inicial."
         case "ligar_ferramenta":
@@ -87,39 +100,39 @@ def execute_comando(comando):
                 caminho.append(acao1["acao1"])
                 acao2 = inquirer.prompt([inquirer.Text("acao2", message="Qual o destino desejado?")])
                 destinos = {
-                    "destino1": destino1,
-                    "destino2": destino2,
-                    "destino3": destino3,
-                    "destino4": destino4,
-                    "destino5": destino5,
-                    "destino6": destino6,
-                    "destino7": destino7,
-                    "destino8": destino8
+                    "destino1": destinos_import['destino1'],
+                    "destino2": destinos_import['destino2'],
+                    "destino3": destinos_import['destino3'],
+                    "destino4": destinos_import['destino4'],
+                    "destino5": destinos_import['destino5'],
+                    "destino6": destinos_import['destino6'],
+                    "destino7": destinos_import['destino7'],
+                    "destino8": destinos_import['destino8']
                 }
 
                 if acao2["acao2"] in destinos:
                     destino.append(acao2["acao2"])
                     remedios = {
-                        "remedio1": remedio1,
-                        "remedio2": remedio2,
-                        "remedio3": remedio3,
-                        "remedio4": remedio4,
-                        "remedio5": remedio5,
-                        "remedio6": remedio6,
-                        "remedio7": remedio7,
-                        "remedio8": remedio8
+                        "remedio1": remedios_import['remedio1'],
+                        "remedio2": remedios_import['remedio2'],
+                        "remedio3": remedios_import['remedio3'],
+                        "remedio4": remedios_import['remedio4'],
+                        "remedio5": remedios_import['remedio5'],
+                        "remedio6": remedios_import['remedio6'],
+                        "remedio7": remedios_import['remedio7'],
+                        "remedio8": remedios_import['remedio8']
                     }
 
                     for posicao in caminho:
                         if posicao in remedios:
                             remedio = remedios[posicao]
                             spinner.start()
-                            device.move_to_J(remedio[0], remedio[1], 55.64, remedio[3], wait=True)
-                            device.move_to_J(remedio[0], remedio[1], remedio[2], remedio[3], wait=True)
+                            device.move_to_J(remedio['x'], remedio['y'], 55.64, remedio['r'], wait=True)
+                            device.move_to_J(remedio['x'], remedio['y'], remedio['z'], remedio['r'], wait=True)
                             device.suck(True)
                             device.wait(200)
-                            device.move_to_J(remedio[0], remedio[1], 55.64, remedio[3], wait=True)
-                            device.move_to_J(home[0], home[1], home[2], home[3], wait=True)
+                            device.move_to_J(remedio['x'], remedio['y'], 55.64, remedio['r'], wait=True)
+                            device.move_to_J(home['x'], home['y'], home['z'], home['r'], wait=True)
                             spinner.stop()
                         else:
                             return "Posição inválida."
@@ -128,13 +141,15 @@ def execute_comando(comando):
                         if chegada in destinos:
                             destino_coords = destinos[chegada]
                             spinner.start()
-                            device.move_to_J(destino_coords[0], destino_coords[1], destino_coords[2], destino_coords[3], wait=True)
+                            device.move_to_J(destino_coords['x'], destino_coords['y'], destino_coords['z'], destino_coords['r'], wait=True)
                             device.suck(False)
                             spinner.stop()
                         else:
                             return "Posição inválida."
                 else:
                     return "Destino inválido."
+                caminho.clear()
+                destino.clear()
             else:
                 return "Remédio inválido."
         case "posicao_atual":
@@ -144,6 +159,10 @@ def execute_comando(comando):
 
 
 if __name__ == "__main__":
+    pontos_home, pontos_remedios, pontos_destinos = load_points_from_file('src/pontos.json')
+    home = pontos_home
+    remedios_import= {ponto['nome']: ponto['posicao'] for ponto in pontos_remedios}
+    destinos_import = {ponto['nome']: ponto['posicao'] for ponto in pontos_destinos}
     if ligar_ferramenta() == "Ferramenta ligada.":
         while True:
             perguntas = [

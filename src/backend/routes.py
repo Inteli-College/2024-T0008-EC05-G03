bc35a1a60e284e8750785372265a9b4924ad1217
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2024-03-20 17:39:15 -0300

    add: download and upload file routes

--- src/backend/routes.py
+++ src/backend/routes.py
@@ -1,9 +1,12 @@
 import os
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file, make_response
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, Text, ForeignKey
 from sqlalchemy.orm import relationship
+from werkzeug.utils import secure_filename
+from io import StringIO
+import csv
 
 current_dir = os.path.dirname(__file__)
 database_uri = 'sqlite:///' + os.path.join(current_dir, '..', 'database', 'database.db')
@@ -65,7 +68,7 @@ def add_compartment(id_layout):
     
     return jsonify({'message': 'compartment added successfully'})
 
-from flask import jsonify
+
 
 # Rota para obter todos os compartimentos de acordo com o layout_id da url
 
@@ -83,6 +86,91 @@ def get_compartments(id_layout):
         return jsonify(compartment_list)
     else:
         return jsonify({'message': 'No compartments found for the given layout ID'}), 404
+    
+@app.route('/download_compartment/<int:id_layout>', methods=['GET'])
+def download_compartment(id_layout):
+    # Adquirir o nome do layout através do id do layout
+    layout = Layout.query.filter_by(id=id_layout).first()
+    
+    # Se não  haver layout
+    if layout is None:
+        return jsonify({'message': 'Layout not found'}), 404
+    
+    nome_layout = layout.nome_layout  
+    
+    # Filtrar todos as linhas da tabela compartment com o id layout igual ao id_layout da URL
+    compartments = Compartment.query.filter_by(id_layout=id_layout).all()
+    if compartments:
+        # Criar um CSV na memória
+        si = StringIO()
+        cw = csv.writer(si)
+        # Escrever o "Header"
+        cw.writerow(['id', 'id_item', 'nome_item', 'numero_compartimento', 'quantidade_item'])
+        # Escrever dados
+        for compartment in compartments:
+            cw.writerow([
+                compartment.id,
+                compartment.id_item,
+                compartment.nome_item,
+                compartment.numero_compartimento,
+                compartment.quantidade_item
+            ])
+        
+        # Resetar o ponteiro da memória do arquivo para o início
+        si.seek(0)
+        
+        # Formatar o nome do layout para adicionar ao arquivo
+        formatted_layout_name = nome_layout.replace(" ", "_")
+        filename = f"{formatted_layout_name}.csv"
+        
+        output = make_response(si.getvalue())
+        output.headers["Content-Disposition"] = f"attachment; filename={filename}"
+        output.headers["Content-type"] = "text/csv"
+        return output
+    else:
+        return jsonify({'message': 'No compartments found for the given layout ID'}), 404
+    
+# Rota para ler arquivo CSV e adicionar a base de dados
+
+@app.route('/upload_compartment', methods=['POST'])
+def upload_compartment():
+    if 'file' not in request.files:
+        return jsonify({'message': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'message': 'No selected file'}), 400
+    if file:
+        filename = secure_filename(file.filename)
+
+        # Pegar o nome do layout através do nome do arquivo
+        layout_name = filename.rsplit('.', 1)[0]
+
+        # Criar uma nova coluna na tabela Layout
+        new_layout = Layout(nome_layout=layout_name)
+        db.session.add(new_layout)
+        db.session.flush()
+        
+        # Ler o arquivo CSV
+        stream = StringIO(file.stream.read().decode("UTF8"), newline=None)
+        csv_reader = csv.DictReader(stream)
+        
+        # Adicionar novas linhas na tabela Compartment
+        for row in csv_reader:
+            new_compartment = Compartment(
+                nome_item=row['nome_item'],
+                quantidade_item=row['quantidade_item'],
+                numero_compartimento=row['numero_compartimento'],
+                id_item=row['id_item'],
+                id_layout=new_layout.id  # Associate with the new layout
+            )
+            db.session.add(new_compartment)
+        
+        db.session.commit()  # Commit all changes to the database
+        
+        return jsonify({'message': 'File uploaded and processed successfully'}), 201
+    
+    return jsonify({'message': 'Error processing file'}), 400
+
     
 # Rota para modificar um compartimento de acordo com a id do compartimento na url
     

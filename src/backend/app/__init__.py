9b4c6b70352ff7f8fea60d4addb45e8da37731ee
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2024-03-21 17:48:18 -0300

    fix: __init__.py

--- src/backend/app/__init__.py
+++ src/backend/app/__init__.py
@@ -16,9 +16,5 @@ def create_app():
     
     db.init_app(app)
     CORS(app)
-    
-    with app.app_context():
-        from . import routes  # Import routes
-        db.create_all()  # Create sql tables for our data models
 
     return app

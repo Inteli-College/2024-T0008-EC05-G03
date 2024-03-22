efe70fd5e9c1a6f6bc8378cae372f4fedd3e0d29
Rafaela Rojas <rafaelacrisr@gmail.com>
2024-03-22 11:32:06 -0300

    add navbar

---
+++ src/frontend/src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from 'src/frontend/src/components/navbar.js';
+
+function App(){
+    return (
+            <React.Fragment>
+                <NavBar/>
+            </React.Fragment>
+    );
+}
+
+export default App;

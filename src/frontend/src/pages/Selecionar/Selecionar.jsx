4ec63097e98d81cd08498570a5e87e73794d116a
Rafaela Rojas <rafaelacrisr@gmail.com>
2024-04-01 15:18:58 -0300

    fix caminhos frontend

--- src/frontend/src/pages/Selecionar/Selecionar.jsx
+++ src/frontend/src/pages/Selecionar/Selecionar.jsx
@@ -1,12 +1,12 @@
 // import '../Selecionado/Selecionado.css'
 import logoCompleta from '../../assets/logo_completa.svg'
 import React from 'react';
-import Voltar from '../../components/voltar';
-import ButtonsPainel from '../../components/ButtonsPainel/ButtonsPainelSelecionar/ButtonsPainelSelecionar';
-import ButtonsPainelSelecionado from '../../components/ButtonsPainel/ButtonsPainelSelecionado/ButtonsPainelSelecionado';
-import ButtonsPainelSelecionar from '../../components/ButtonsPainel/ButtonsPainelSelecionar/ButtonsPainelSelecionar';
+import Voltar from '../../components/voltar/voltar.jsx';
+// import ButtonsPainel from '../../components/buttonsPainel';
+import ButtonsPainelSelecionado from '../../components/buttonsPainel/ButtonsPainelSelecionado/ButtonsPainelSelecionado';
+import ButtonsPainelSelecionar from '../../components/buttonsPainel/ButtonsPainelSelecionar/ButtonsPainelSelecionar';
 
-function Selecionar() {
+function Selecionar() { 
     return (
         <div className='pageContent'>
             <div className='layoutsContainer'>

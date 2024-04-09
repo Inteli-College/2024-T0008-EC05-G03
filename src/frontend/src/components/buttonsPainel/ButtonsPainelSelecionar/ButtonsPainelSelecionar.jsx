a3204272305ed97d1e8c654dd099177df5fa65ba
Antonio <antonio.guimaraes@sou.inteli.edu.br>
2024-04-09 15:42:58 -0300

    add import layout in selecionado

--- src/frontend/src/components/buttonsPainel/ButtonsPainelSelecionar/ButtonsPainelSelecionar.jsx
+++ src/frontend/src/components/buttonsPainel/ButtonsPainelSelecionar/ButtonsPainelSelecionar.jsx
@@ -1,13 +1,48 @@
 import React from "react";
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import Voltar from "../../voltar/voltar.jsx";
 import './ButtonsPainelSelecionar.css';
 import logoCompleta from '../../../assets/logo_completa.svg';
 import ModalAdicionar from "../../modalAdicionar/modalAdicionar.jsx";
 
 const ButtonsPainelSelecionar = ({ onExportClick, onEditClick }) => {
-
+    const [selectedFile, setSelectedFile] = useState(null);
+    const fileInputRef = useRef(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    }
+
+    const handleUpload = async () => {
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append('file', selectedFile);
+
+            try {
+                const response = await fetch('/upload_compartment', {
+                    method: 'POST',
+                    body: formData
+                });
+                if (response.ok) {
+                    alert('File uploaded successfully!');
+                    // Optionally, you can do something after successful upload
+                } else {
+                    alert('Error uploading file');
+                }
+            } catch (error) {
+                console.error('Error uploading file:', error);
+                alert('Error uploading file');
+            }
+        } else {
+            alert('Please select a file');
+        }
+    }
+
+    const handleClick = () => {
+        fileInputRef.current.click();
+    }
+
     const click = () => {
         setIsModalOpen(true);
     }
@@ -27,7 +62,13 @@ const ButtonsPainelSelecionar = ({ onExportClick, onEditClick }) => {
                 onExportClick(prevState => !prevState);
             }}></button>
         </form>
-        <form action='/importarlayout'><button className='botaoPadrao'></button></form>
+        <button className='botaoPadrao' onClick={handleClick}>Importar Layout</button>
+        <input
+                    type="file"
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                    style={{ display: "none" }}
+        />
         {/* <form action='/editarlayout'>
             <button type='button' className='botaoPadrao' onClick={(event) => {
                 event.preventDefault();
